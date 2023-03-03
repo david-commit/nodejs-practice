52a4c750b283f4c4af319032609f2e0f813ec668
david-commit <davidondiege@gmail.com>
2023-03-03 13:33:52 +0300

    Sync/Async: readFileSync

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+
+const fileName = './lorem.txt'
+
+const errorHandler = err => console.log(err)
+const dataHandler = data => console.log(data.toString())
+fs.readFileSync(fileName, (err, data) => {
+ if (err) errorHandler(err)
+ dataHandler(data)
+})
+
+app.get('/', (req, res) => {
+ res.send('Hello Node JS')
+})
+
+console.log("I'm learning NodeJS")
 
 const PORT = 3000
 app.listen(PORT, () => console.log(`Node server running on PORT:${PORT}`))
