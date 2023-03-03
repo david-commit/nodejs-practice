fab8d3901740872791b65f7be97340e0c53fc7eb
david-commit <davidondiege@gmail.com>
2023-03-03 13:50:36 +0300

    Added a custom middleware

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
+const morgan = require('morgan')
+const { getPosts } = require('./routes/post')
 
 const fileName = './lorem.txt'
-
 const errorHandler = err => console.log(err)
 const dataHandler = data => console.log(data.toString())
 fs.readFileSync(fileName, (err, data) => {
@@ -11,11 +12,19 @@ fs.readFileSync(fileName, (err, data) => {
  dataHandler(data)
 })
 
-app.get('/', (req, res) => {
- res.send('Hello Node JS')
-})
+// Custom Middleware
+const customMiddleware = (req, res, next) => {
+ console.log('Middleware😂')
+ next()
+}
+
+// Middleware
+app.use(morgan('dev'))
+
+// Route
+app.get('/', customMiddleware, getPosts)
 
 console.log("I'm learning NodeJS")
 
-const PORT = 3000
+const PORT = 8080
 app.listen(PORT, () => console.log(`Node server running on PORT:${PORT}`))
